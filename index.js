3ccb79660cad1c317aad6512a40790e71dee31f8
Divan Visagie <me@dvisagie.com>
2020-08-23 19:40:39 +0200

    redux

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const bonjour = require('bonjour')()
 const express = require('express')
+const { createStore } = require('redux')
 const app = express()
 
-const PORT = 3000;
+
+const PORT = 3000
 
 bonjour.find({ type: 'gru' }, (service) => {
     console.log('found a gru server', service)
 })
+
+//Store
+const state = {
+    led: {
+        pin: 13,
+        value: 0
+    }
+};
+
+function arduino(state = state, action) {
+    switch (action.type) {
+        case 'LED_TOGGLE':
+            if (state.led.value === 0)
+                state.led.value = 255
+            if (state.led.value === 255)
+                state.led.value = 0
+            return state
+        default:
+            return state
+    }
+}
+
+let store = createStore(arduino)
 
 //Server
 app.get('/led', (req, res) => {
@@ -20,3 +45,6 @@ app.listen(PORT, () => {
         port: PORT
     })
 })
+
+//Board
+
