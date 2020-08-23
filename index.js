589498ab7d48f2661c8ca28cf5363132da3ab7b0
Divan Visagie <me@dvisagie.com>
2020-08-23 19:28:13 +0200

    add express

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const bonjour = require('bonjour')();
+const bonjour = require('bonjour')()
+const express = require('express')
+const app = express()
 
-const port = 3000;
-
-bonjour.publish({
-    name: 'Gru Minion Manager',
-    type: 'gru',
-    port
-});
+const PORT = 3000;
 
 bonjour.find({ type: 'gru' }, (service) => {
     console.log('found a gru server', service)
-});
+})
 
-bonjour.find({ type: '' }, (service) => {
-    console.log('>> server', service)
-});
+//Server
+app.get('/led', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(PORT, () => {
+    bonjour.publish({
+        name: 'Gru Minion Manager',
+        type: 'gru',
+        port: PORT
+    })
+})
